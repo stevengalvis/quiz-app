8ccfced343592f30e305fbfdf96858648ba5f4fb
stevengalvis <steven.galvis@gmail.com>
2017-04-25 08:31:39 -0400

    added get answer function and check answer

--- app.js
+++ app.js
@@ -11,6 +11,36 @@ function addQuestion(state,question,answer,choices){
         });
 }
 
+//get answer
+function getAnswer(state,questionId){
+    return state.questions[questionId].answer
+}
+
+function checkAnswer(state,questionId,userAnswer){
+    return getAnswer(state,questionId) === userAnswer;
+}
+
+function createQuestion(state,questionId){
+
+}
+
+
+
+//function that keeps track of total questions
+//function that keeps track of total correct answers
+
+//event handler for submit button
+//event handler for updating total questions and correct answers
+
+//function for when quiz is done display results(dom manipulaiton)
+
+
+
+
+
+
+
+//adding questions to the quiz
 addQuestion(state,'What does Snuffles want to be called now that he abandoned his slave name?','Snowball',['Snowball','Sniffles','Snoopy','Kimbel','Buddy']);
 addQuestion(state,'What is the name of Rick\'s favorite intergalactic arcade?','Blips and Chitz',['Dimension C-137','Intergalactic Fun Land', 'Anatomy Park', 'Blips and Chitz','Smith Residence']);
 addQuestion(state,'In the episode "Big Trouble in Little Sanchez" what is the name of the younger version of Rick?','Tiny Rick',['Rickest Rick','Evil Rick','Little Sanchez','Tiny Rick','Doofus Rick']);
@@ -21,5 +51,3 @@ addQuestion(state,"Which of these planets did the Smith family visit to escape t
 addQuestion(state,'Name of the planet Rick and Morty visit where the females have evolved to become super intelligent and rule the world?','Gazorpazorp',['Purge Planet','Gazorpazorp','Planet Squanch','Arbolez Mentirosos','Dimension C-137']);
 
 console.log(state.questions);
-
-var questionTemplate;
