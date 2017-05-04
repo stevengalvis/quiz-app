643a908db0a375e3f9c118f50cd72122c886f47b
stevengalvis <steven.galvis@gmail.com>
2017-05-03 20:08:46 -0400

    cleaned up the app deleted a few console.logs

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ var state = {
     questionCount: 0,
 };
 
-function addQuestion(state, question, answer, choices, renderAnswer, imageQuestion, imageAnswer) {
+function addQuestion(state, question, answer, choices, imageQuestion, imageAnswer) {
     state.questions.push({
         question: question,
         answer: answer,
         choices: choices,
-        renderAnswer: renderAnswer,
         imageQuestion: imageQuestion,
         imageAnswer: imageAnswer
     });
@@ -51,24 +50,24 @@ function renderQuestion(state, questionId) {
     $('.js-start').addClass('visuallyhidden');
 }
 
-function renderAnswer(state, questionId,userAnswer) {
+function renderAnswer(state, questionId, userAnswer) {
     var question = state.questions[questionId];
     var element = $('.js-quiz-layout');
     element.find('.js-main-image').attr('src',question.imageAnswer);
     correctAnswer = checkAnswer(state,questionId,userAnswer);
+
     if (correctAnswer){
         state.correctAnswers += 1;
         element.find('i').removeClass('visuallyhidden');
         element.find('i').removeClass("fa fa-times fa-5x");
         element.find('i').addClass("fa fa-check fa-5x");
-
     }
-
     else {
         element.find('i').removeClass('visuallyhidden');
         element.find('i').removeClass('fa fa-check fa-5x');
         element.find('i').addClass("fa fa-times fa-5x");
     }
+
     element.find('.js-card-text').text("The correct Answer is : " + question.answer);
     $('.js-start').addClass('visuallyhidden');
     $('.js-next').removeClass('visuallyhidden');
@@ -85,13 +84,13 @@ function renderResults(state){
 }
 
 //questions with answers for quiz
-addQuestion(state, 'What does Snuffles want to be called now that he abandoned his slave name?', 'Snowball', ['Snowball', 'Sniffles', 'Snoopy', 'Kimbel'], 'In the episode "Lawnmower Dog" Rick gives the dog Snuffles a device that enhances intelligence. After he eventually becomes smart enough and realizes he doesn\'t like his pet name "Snuffles", he changes his name to Snowball and back again later in the episode.', 'images/snuffles.jpg', 'images/snowballs.gif');
-addQuestion(state, 'What is the name of Rick\'s favorite intergalactic arcade?', 'Blips and Chitz', ['Dimension C-137', 'Intergalactic Fun Land', 'Anatomy Park', 'Blips and Chitz'], 'In the episode "Mortynight Run", Rick sells a gun to an alien assasin(Krombopulos Michael) so he can have enough money to afford an afternoon at the intergalactic arcade "Blips and Chitz".', 'images/blips-chitz.jpg', 'images/rick-morty-blitz-chips.gif');
-addQuestion(state, 'What is the name of the younger version of Rick?', 'Tiny Rick', ['Rickest Rick', 'Evil Rick', 'Little Sanchez', 'Tiny Rick'], "In the episode \"Big Trouble in Little Sanchez\" Tiny Rick is a younger version of Rick. Rick transfers his brain into a younger clone of himself in an attempt to catch a vampire at Harry Herpson High School with Morty and Summer.", 'images/tinyrick.png', 'images/tiny_rick.gif');
-addQuestion(state, 'What is the name of the virtual reality game that Rick and Morty play at Blips and Chitz?', 'Roy', ['Mortynight Run', 'Brian', 'Roy', 'Pocket Mortys'], 'Roy: A Life Well Lived is a virtual reality life simulator game played at Blips and Chitz by Rick and Morty. In the game the main character is Roy, whom you must guide through life\'s challenges, and the players final score is how many years Roy lived.', 'images/rick-and-morty-roy-blips-and-chitz.jpg', 'images/rick-morty-roy.gif');
-addQuestion(state, 'What does Rick\'s catchphrase "Wubba lubba dub dub!" mean in native bird person language?', "Help me. I am in great pain", ['Get Schwifty', 'Cronenberged up the whole place', 'Monument to Compromise', 'Help me. I am in great pain'], "Rick usually says Wubba lubba dub dub! when he\'s in a good mood or after he cracks a joke. However, in season one episode titled 'Ricksy Business' we find out it means \"help me. I am in great pain\" in the Birdperson language.", 'images/wubba.jpg', 'images/wubba.gif');
-addQuestion(state, "Which of these planets did the Smith family visit to escape the Federal Jurisdiction?", 'On a Cob Planet', ['Gear World', 'Hideout Planet', 'On a Cob Planet', 'Alphabetrium'], "The Smith family moved to this planet when they were trying to escape the Federal Jurisdiction. Everything is on a cob, even mountains, down to the molecular level. When Rick realizes this he was disturbed and urges the family to evacuate the planet.", 'images/smith-family.jpg', 'images/cob-planet.gif');
-addQuestion(state, 'Name of the planet Rick and Morty visit where the females have evolved to become super intelligent and rule the world?', 'Gazorpazorp', ['Purge Planet', 'Gazorpazorp', 'Planet Squanch', 'Dimension C-137'], "In the episode \"Raising Gazorpazorp\" Rick, Summer, and Morty meet Gazorpians known for their large humanoid figures with six hands. Due to conflicts the male and female species of Gazorpazorp became divided. The females thrived and lead the world while the males remained primitive.", 'images/gazorpazorp.png', 'images/gazorpazorp.gif');
+addQuestion(state, 'What does Snuffles want to be called now that he abandoned his slave name?', 'Snowball', ['Snowball', 'Sniffles', 'Snoopy', 'Kimbel'], 'images/snuffles.jpg', 'images/snowballs.gif');
+addQuestion(state, 'What is the name of Rick\'s favorite intergalactic arcade?', 'Blips and Chitz', ['Dimension C-137', 'Intergalactic Fun Land', 'Anatomy Park', 'Blips and Chitz'], 'images/blips-chitz.jpg', 'images/rick-morty-blitz-chips.gif');
+addQuestion(state, 'What is the name of the younger version of Rick?', 'Tiny Rick', ['Rickest Rick', 'Evil Rick', 'Little Sanchez', 'Tiny Rick'], 'images/tinyrick.png', 'images/tiny_rick.gif');
+addQuestion(state, 'What is the name of the virtual reality game that Rick and Morty play at Blips and Chitz?', 'Roy', ['Mortynight Run', 'Brian', 'Roy', 'Pocket Mortys'], 'images/rick-and-morty-roy-blips-and-chitz.jpg', 'images/rick-morty-roy.gif');
+addQuestion(state, 'What does Rick\'s catchphrase "Wubba lubba dub dub!" mean in native bird person language?', "Help me. I am in great pain", ['Get Schwifty', 'Cronenberged up the whole place', 'Monument to Compromise', 'Help me. I am in great pain'], 'images/wubba.jpg', 'images/wubba.gif');
+addQuestion(state, "Which of these planets did the Smith family visit to escape the Federal Jurisdiction?", 'On a Cob Planet', ['Gear World', 'Hideout Planet', 'On a Cob Planet', 'Alphabetrium'], 'images/smith-family.jpg', 'images/cob-planet.gif');
+addQuestion(state, 'Name of the planet Rick and Morty visit where the females have evolved to become super intelligent and rule the world?', 'Gazorpazorp', ['Purge Planet', 'Gazorpazorp', 'Planet Squanch', 'Dimension C-137'], 'images/gazorpazorp.png', 'images/gazorpazorp.gif');
 
 
 //event listeners
@@ -108,17 +107,12 @@ $('form').on('change', 'input', function(e){
 $('.btn-controls').on('click','.js-next',function(event){
     var questionId = state.questionCount;
     if(questionId >= state.questions.length){
-        console.log('done');
         renderResults(state);
     }
-    console.log(questionId);
-    console.log(state.questions.length);
+
     renderQuestion(state,questionId);
-
-
 })
 
-    //start the quiz
 $(function() {
 
     $('.js-start').on('click',function(event){
